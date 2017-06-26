9af117596519cd2056b0ca9a9e812f1d4dddbd63
dkx1982 <dkx1982@163.com>
2017-06-27 05:31:41 +0800

    06 interger positive

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -3,8 +3,19 @@
 
 print "请输入一个整数，然后按 Enter: "
 x = gets
-
+if x.to_i % 2 == 0
+  v = "偶数"
+else
+  v = "奇数"
+end
+if x.to_i > 0
+  d = "正数"
+elsif x.to_i < 0
+  d = "负数"
+else
+  d = "零"
+end
 # ....
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+puts "这个数是#{d}"
+puts "这个数是#{v}"
